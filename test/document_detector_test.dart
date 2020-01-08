f9c44bff22fe3b0b3d06239f1e85eb8e2dec6d61
Lav Shinde <shindelav@gmail.com>
2020-01-08 17:35:43 +0900

    Change Test Case

--- test/document_detector_test.dart
+++ test/document_detector_test.dart
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await DocumentDetector.platformVersion, '42');
+    expect(await DocumentDetector.detect('asdf'), '42');
   });
 }
