0b89caad95533e062a5b18d69b4b1899d73d4ae3
Lav Shinde <shindelav@gmail.com>
2020-01-09 15:32:27 +0900

    Add OpenCV dependency (via JavaCPP)

--- android/src/main/kotlin/com/tnexta/document_detector/ImageLoader.kt
+++ android/src/main/kotlin/com/tnexta/document_detector/ImageLoader.kt
@@ -1,7 +1,13 @@
 package com.tnexta.document_detector
 
+import org.bytedeco.javacpp.Loader
+import org.bytedeco.opencv.global.opencv_dnn
+
 object ImageLoader {
   fun load(imagePath: String): String {
-    return imagePath
+    // Loader.load(opencv_java)
+    val ver : String = opencv_dnn.OPENCV_DNN_API_VERSION.toString()
+    return ver
+    // return imagePath
   }
 }
