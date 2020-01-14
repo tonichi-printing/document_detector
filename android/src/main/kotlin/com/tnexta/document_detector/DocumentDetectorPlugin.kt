80d857d61a79cdffde4ae3551962644e6ebcac68
Lav Shinde <shindelav@gmail.com>
2020-01-14 11:55:52 +0900

    Load the Image

--- android/src/main/kotlin/com/tnexta/document_detector/DocumentDetectorPlugin.kt
+++ android/src/main/kotlin/com/tnexta/document_detector/DocumentDetectorPlugin.kt
@@ -8,6 +8,9 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
+import org.bytedeco.javacpp.Loader
+import org.opencv.core.Mat
+
 /** DocumentDetectorPlugin */
 public class DocumentDetectorPlugin: FlutterPlugin, MethodCallHandler {
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
@@ -34,12 +37,32 @@ public class DocumentDetectorPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "detectDocument") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}" + call.argument("imagePath"))
+      Loader.load(org.bytedeco.opencv.opencv_java::class.java)
+
+      detectDocument(call, result)
     } else {
       result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  }
+
+  fun detectDocument(@NonNull call: MethodCall, @NonNull result: Result) {
+    var imagePath : String? = call.argument("imagePath")
+    if (imagePath == null) {
+      result.error("No image path provided", null, null)
+    }
+    // result.success(org.opencv.core.Core.getBuildInformation());
+    // Reference: https://github.com/legolas123/cv-tricks.com/blob/master/OpenCV/Edge_detection/edge.py
+    try {
+      val image : Mat = ImageLoader.load(imagePath.toString());
+      result.success((image.empty()).toString())
+  } catch (e: IllegalArgumentException) {
+      result.error(e.message, null, null)
+    }
+    // result.success((image.dims()).toString())
+    // val documentCoordinates = CannyDetector.hed(image)
+    // result.success((documentCoordinates.dims()).toString())
   }
 }
